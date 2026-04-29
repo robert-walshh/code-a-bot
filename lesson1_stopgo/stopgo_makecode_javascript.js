2132b21bdeb2dbe3d910f60bd8de9cc77a717b97
Robert Walsh <20102479@mail.wit.ie>
2026-04-29 16:32:04 +0100

    Lesson 1: Stop/Go Vision System - Final Demo Code
    Added in Reverse and Nothing Options.
    All movement is based around 1500 as 0.

--- lesson1_stopgo/stopgo_makecode_javascript.js
+++ lesson1_stopgo/stopgo_makecode_javascript.js
@@ -1,30 +1,27 @@
-/*
-
-2026, Robert Walsh
-Copy and Paste this into the MakeCode Micro:Bit JavaScript Editor on your Web Browser
-Micro:Bit Extensions Required: bluetooth
-Services Required: LOFI Robots Teachable Micro:Bit
-Teachable Machine Sample Model URL:
-
-*/
-
-bluetooth.onUartDataReceived(
-  serial.delimiters(Delimiters.NewLine),
-  function on_uart_data_received() {
-    receivedString = bluetooth.uartReadUntil(
-      serial.delimiters(Delimiters.NewLine),
-    );
-  },
-);
-
-let receivedString = "";
-bluetooth.startUartService();
+//  2026, Robert Walsh
+//  Copy and Paste this into the MakeCode Micro:Bit Python Editor on your Web Browser
+//  Micro:Bit Extensions Required: bluetooth
+//  Services Required: LOFI Robots Teachable Micro:Bit
+//  Teachable Machine Sample Model URL:
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function on_uart_data_received() {
+    
+    receivedString = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+})
+let receivedString = ""
+bluetooth.startUartService()
 basic.forever(function on_forever() {
-  if (receivedString == "go") {
-    pins.servoSetPulse(AnalogPin.P1, 500);
-    pins.servoSetPulse(AnalogPin.P2, 500);
-  } else if (receivedString == "stop") {
-    pins.servoSetPulse(AnalogPin.P1, 0);
-    pins.servoSetPulse(AnalogPin.P2, 0);
-  }
-});
+    if (receivedString == "go") {
+        pins.servoSetPulse(AnalogPin.P1, 1300)
+        pins.servoSetPulse(AnalogPin.P2, 1300)
+    } else if (receivedString == "stop") {
+        pins.servoSetPulse(AnalogPin.P1, 1500)
+        pins.servoSetPulse(AnalogPin.P2, 1500)
+    } else if (receivedString == "reverse") {
+        pins.servoSetPulse(AnalogPin.P1, 1700)
+        pins.servoSetPulse(AnalogPin.P2, 1700)
+    } else if (receivedString == "nothing") {
+        pins.servoSetPulse(AnalogPin.P1, 1500)
+        pins.servoSetPulse(AnalogPin.P2, 1500)
+    }
+    
+})
--- lesson1_stopgo/stopgo_makecode_python.py
+++ lesson1_stopgo/stopgo_makecode_python.py
@@ -1,34 +1,29 @@
-"""
-
-2026, Robert Walsh
-Copy and Paste this into the MakeCode Micro:Bit Python Editor on your Web Browser
-Micro:Bit Extensions Required: bluetooth
-Services Required: LOFI Robots Teachable Micro:Bit
-Teachable Machine Sample Model URL:
-
-"""
-
+# 2026, Robert Walsh
+# Copy and Paste this into the MakeCode Micro:Bit Python Editor on your Web Browser
+# Micro:Bit Extensions Required: bluetooth
+# Services Required: LOFI Robots Teachable Micro:Bit
+# Teachable Machine Sample Model URL:
 
 def on_uart_data_received():
     global receivedString
     receivedString = bluetooth.uart_read_until(serial.delimiters(Delimiters.NEW_LINE))
-
-
-bluetooth.on_uart_data_received(
-    serial.delimiters(Delimiters.NEW_LINE), on_uart_data_received
-)
+bluetooth.on_uart_data_received(serial.delimiters(Delimiters.NEW_LINE),
+    on_uart_data_received)
 
 receivedString = ""
 bluetooth.start_uart_service()
 
-
 def on_forever():
     if receivedString == "go":
-        pins.servo_set_pulse(AnalogPin.P1, 500)
-        pins.servo_set_pulse(AnalogPin.P2, 500)
+        pins.servo_set_pulse(AnalogPin.P1, 1300)
+        pins.servo_set_pulse(AnalogPin.P2, 1300)
     elif receivedString == "stop":
-        pins.servo_set_pulse(AnalogPin.P1, 0)
-        pins.servo_set_pulse(AnalogPin.P2, 0)
-
-
+        pins.servo_set_pulse(AnalogPin.P1, 1500)
+        pins.servo_set_pulse(AnalogPin.P2, 1500)
+    elif receivedString == "reverse":
+        pins.servo_set_pulse(AnalogPin.P1, 1700)
+        pins.servo_set_pulse(AnalogPin.P2, 1700)
+    elif receivedString == "nothing":
+        pins.servo_set_pulse(AnalogPin.P1, 1500)
+        pins.servo_set_pulse(AnalogPin.P2, 1500)
 basic.forever(on_forever)
